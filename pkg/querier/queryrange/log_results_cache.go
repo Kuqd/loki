318ddef28646a271b1802ca052e97e3767189da5
Cyril Tovena <cyril.tovena@gmail.com>
2020-03-27 14:58:47 -0400

    wip
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/querier/queryrange/log_results_cache.go
+++ pkg/querier/queryrange/log_results_cache.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	proto "github.com/gogo/protobuf/proto"
 
 	"github.com/cortexproject/cortex/pkg/chunk/cache"
 	"github.com/cortexproject/cortex/pkg/querier/queryrange"
@@ -43,4 +45,17 @@ func NewLogResultsCacheMiddleware(
 
 func (s logResultsCache) Do(ctx context.Context, r queryrange.Request) (queryrange.Response, error) {
 	return s.next.Do(ctx, r)
+}
+
+func (s logResultsCache) put(ctx context.Context, key string, extents []queryrange.Extent) {
+	buf, err := proto.Marshal(&queryrange.CachedResponse{
+		Key:     key,
+		Extents: extents,
+	})
+	if err != nil {
+		level.Error(s.logger).Log("msg", "error marshalling cached value", "err", err)
+		return
+	}
+
+	s.cache.Store(ctx, []string{cache.HashKey(key)}, [][]byte{buf})
 }
